b290a2462e24eb3a09c54a0c64fb7684ce40b6a1
jatinkalra070801 <59727159+jatinkalra070801@users.noreply.github.com>
2020-01-24 21:44:24 +0530

    Update perm_and_comb.cpp
    completed p-n- problem

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -3,15 +3,47 @@ using namespace std;
 
 int permutation(int n, int r)
 {
-  return 0;
+    if(r==0)
+	return 1;
+	else
+	return n*permutation(n-1,r-1);
 }
 
 int combination(int n, int r)
 {
-  return 0;
+    if(r==0)
+	return 1;
+	else
+	return 1.0*n*combination(n-1,r-1)/r;
 }
 
 int main()
 {
+  	int n,r,per,com;
+	cout<<"enter value of n and r\n";
+	cin>>n>>r;
+	if(r<=n){
+		try
+		{
+			if(n<0||r<0)
+			throw n;
+			else if(r==0)
+			{
+				per=1;com=1;
+			}
+			else
+			{cout<<"permutation of the given input is\n"<<permutation(n,r);
+			cout<<"combination of the given input is\n"<<combination(n,r);
+			}
+		}
+		catch(int y)
+		{
+			cout<<"invalid";
+		}
+		
+	}
+	else
+	{cout<<"invalid input from the user";
+	}
   return 0;
 }
